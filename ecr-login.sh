10c51399c9d36b659a00aa78b9f25d9428e3863e
aravindramini <92906308+aravindramini@users.noreply.github.com>
2022-01-13 14:24:53 +0530

    Delete ecr-login.sh

--- ecr-login.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 164566612831.dkr.ecr.us-west-1.amazonaws.com
-docker build -t myrepo .
-docker tag myrepo:latest 164566612831.dkr.ecr.us-west-1.amazonaws.com/myrepo:latest
-docker tag myrepo:latest 164566612831.dkr.ecr.us-west-1.amazonaws.com/myrepo:1.0
-docker push 164566612831.dkr.ecr.us-west-1.amazonaws.com/myrepo:latest
-docker push 164566612831.dkr.ecr.us-west-1.amazonaws.com/myrepo:1.0
